70d4bbc8d66e947ab354fe5515dc4d747384b36c
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-08-05 14:06:04 -0500

    when you click, the label moves to wherever you clicked within the label (with math adjusted, finally!)

--- drawStuff2/ViewController.swift
+++ drawStuff2/ViewController.swift
@@ -9,16 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var yellowSquare: UIView!
+    @IBOutlet weak var tealSquare: UILabel!
+    let displaySize: CGRect = UIScreen.main.bounds
+    var location = CGPoint(x:300, y:400)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(tealSquare.center)
+        
     }
 
+    @IBAction func onTappedYellowSquare(_ sender: UITapGestureRecognizer) {
+        tealSquare.center = location
+        
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            location = touch.location(in: yellowSquare)
+            location.x += 37.5
+            location.y += 183.5
+            print(touch)
+            print(location)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
