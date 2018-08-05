355efb4c914246399ec91d426d94c0a31895c217
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-08-05 14:40:15 -0500

    got rid of unnecessary things like the yellow square view so it's just drawing on a blank screen

--- drawStuff2/ViewController.swift
+++ drawStuff2/ViewController.swift
@@ -9,47 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var yellowSquare: UIView!
-    @IBOutlet weak var tealSquare: UILabel!
-    let displaySize: CGRect = UIScreen.main.bounds
-    var location = CGPoint(x:300, y:400)
+    
+    @IBOutlet var regularView: UIView!
+    var location = CGPoint(x:0, y:0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(tealSquare.center)
         
-    }
-
-    @IBAction func onTappedYellowSquare(_ sender: UITapGestureRecognizer) {
-        
-        
-    }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            location = touch.location(in: yellowSquare)
-            location.x += 37.5
-            location.y += 183.5
-            print(touch)
-            print(location)
-        }
-    }
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        for touch in touches{
-            location = touch.location(in: yellowSquare)
-            location.x += 37.5
-            location.y += 183.5
-            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
-            label.center = location
-            label.textAlignment = .center
-            label.backgroundColor = .purple
-            self.view.addSubview(label)
-        }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+            for touch in touches{
+                location = touch.location(in: regularView)
+                let label = UILabel(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+                label.center = location
+                label.textAlignment = .center
+                label.backgroundColor = .purple
+                self.view.addSubview(label)
+            }
+        }
+
     
     
 
