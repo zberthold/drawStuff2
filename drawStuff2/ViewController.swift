ec47c12190306773275ee4ad9b3b4a09784c8e9f
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-08-05 14:29:41 -0500

    when you drag it creates a label, making drawing

--- drawStuff2/ViewController.swift
+++ drawStuff2/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onTappedYellowSquare(_ sender: UITapGestureRecognizer) {
-        tealSquare.center = location
+        
         
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -31,6 +31,19 @@ class ViewController: UIViewController {
             location.y += 183.5
             print(touch)
             print(location)
+        }
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in touches{
+            location = touch.location(in: yellowSquare)
+            location.x += 37.5
+            location.y += 183.5
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+            label.center = location
+            label.textAlignment = .center
+            label.backgroundColor = .purple
+            self.view.addSubview(label)
         }
     }
     override func didReceiveMemoryWarning() {
